dd3673af74afc9fb6cc72b1c16961ab76be4cd76
nreinhol <nreinhold24@gmail.com>
2019-03-20 15:35:50 +0100

    add first decomposition stuff

--- pyapp/src/lem_sim/linear_optimization/decompose.py
+++ pyapp/src/lem_sim/linear_optimization/decompose.py
@@ -1,2 +1,13 @@
 AMOUNT_AGENTS = None
 AMOUNT_AGENTS_VARIABLES = None
+
+target_coefs = [-1, -2, -1, -3]
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+for chunk in chunks(target_coefs, 2):
+    print(chunk)
