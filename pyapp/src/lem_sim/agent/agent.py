31818cce13461cd35c111cfb0efe48e5f9ba16ae
Niklas Reinhold <nreinhold24@gmail.com>
2019-03-28 22:17:52 +0100

    add optimization problem to agent class

--- pyapp/src/lem_sim/agent/agent.py
+++ pyapp/src/lem_sim/agent/agent.py
@@ -4,6 +4,7 @@ class Agent():
     def __init__(self, account_address, provider):
         self._account_address = account_address
         self._provider = provider
+        self._optimization_problem = None
 
     @property
     def account_address(self):
@@ -12,6 +13,14 @@ class Agent():
     @property
     def balance(self):
         return self._provider.eth.getBalance(self.account_address)
+
+    @property
+    def optimization_problem(self):
+        return self._optimization_problem
+
+    @optimization_problem.setter
+    def optimization_problem(self, value):
+        self._optimization_problem = value
 
     def send_transaction(self, receiver_account_address, value):
         self._provider.eth.sendTransaction({'to': receiver_account_address, 'from': self.account_address, 'value': value})
