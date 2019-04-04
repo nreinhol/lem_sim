92023aaf6ddd16265834570bca2407cd134a5cd6
nreinhol <nreinhold24@gmail.com>
2019-04-04 14:02:50 +0200

    do further work on decomposition algo

--- pyapp/src/lem_sim/linearoptimization/decompose.py
+++ pyapp/src/lem_sim/linearoptimization/decompose.py
@@ -9,25 +9,39 @@ def decompose(central_problem, var):
         c = shared resources
         C = shared coefs '''
 
-        d, n, N, C = split_central_problem(central_problem, var.amount_agents)
-        c = central_problem.shared_resources
-        C = remove_zero_rows_of_individual_coefs(C)
+        d, n, N, c, C = split_central_problem(central_problem, var.amount_agents)
 
-        print(c.tolist())
+        N = remove_zero_rows_of_individual_coefs(N)
+
+        constraint_coefs = build_constraint_coefs_for_agents(N, C)
+        constraint_bounds = build_constraint_bounds_for_agents(n, c)
+
+        for d_j, A_j, u_j in zip(d, constraint_coefs, constraint_bounds):
+                print(d_j, A_j, u_j)
 
 
 def split_central_problem(central_problem, amount_agents):
         d = np.split(central_problem.target_coefs, amount_agents, axis=0)
         n = np.split(central_problem.individual_resources, amount_agents, axis=0)
         N = np.split(central_problem.individual_coefs, amount_agents, axis=1)
+        c = distribute_shared_resources(central_problem.shared_resources, amount_agents)
         C = np.split(central_problem.shared_coefs, amount_agents, axis=1)
 
-        return d, n, N, C
+        return d, n, N, c, C
 
 
 def remove_zero_rows_of_individual_coefs(N):
-        N_removed = []
-        for N_j in N:
-                N_removed.append(N_j[~np.all(N_j == 0, axis=1)])
+        return [N_j[~np.all(N_j == 0, axis=1)] for N_j in N]
 
-        return N_removed
+
+def distribute_shared_resources(c, amount_agents):
+        ''' distribute the shared resources into equally sized parts '''
+        return [np.divide(c, amount_agents + 1) for i in range(amount_agents)]
+
+
+def build_constraint_coefs_for_agents(N, C):
+        return [np.concatenate((N_j, C_j)) for N_j, C_j in zip(N, C)]
+
+
+def build_constraint_bounds_for_agents(n, c):
+        return [np.concatenate((n_j, c_j)) for n_j, c_j in zip(n, c)]
