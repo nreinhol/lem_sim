051ec3fa72d91de6e1e2d3bbd7dd547c05e64f1d
nreinhol <nreinhold24@gmail.com>
2019-04-05 18:00:56 +0200

    adjust header output

--- pyapp/src/lem_sim/output/draw.py
+++ pyapp/src/lem_sim/output/draw.py
@@ -2,7 +2,7 @@ import click as c
 
 
 def print_central_problem(central_problem):
-        c.secho('\n Central LP ', bg='white', fg='black')
+        c.secho('\nCentral LP ', bg='white', fg='black')
         central_problem.show()
 
 
@@ -13,7 +13,7 @@ def print_agents_lps(variables, decompose=True):
                 c.secho('decompose and distribute to:\n', bold=True)
 
         for index, agent in enumerate(variables.agent_pool):
-                c.secho('\n LP of Agent {} '.format(index + 1), bg='green', fg='white')
+                c.secho('\nLP of Agent {} '.format(index + 1), bg='green', fg='white')
                 agent.optimization_problem.show()
 
         c.secho('_______________________________')
