f72d3b29a9365d5f9479df16b9fcf6f6a10caee7
nreinhol <nreinhold24@gmail.com>
2019-04-05 16:44:40 +0200

    start with log package

--- pyapp/src/lem_sim/simulate.py
+++ pyapp/src/lem_sim/simulate.py
@@ -5,21 +5,12 @@ import click
 
 
 @click.command()
-@click.option('--connection', type=click.Choice(['docker', 'ip']))
+@click.option('--connection', type=click.Choice(['docker', 'ip']), default='docker')
 def main(connection):
+
     variables = mem.Variables(connection)
     central_problem = lp.CentralProblem()
     lp.decompose(central_problem, variables)
-
-    '''
-    # get two specific agents out of list
-    agent_one = variables.agent_pool[0]
-    agent_two = variables.agent_pool[1]
-
-    print('balance before tx: {}'.format(agent_two.balance))
-    agent_one.send_transaction(agent_two.account_address, 1000000000000000000)
-    print('balance after tx: {}'.format(agent_two.balance))
-    '''
 
 
 if __name__ == '__main__':
