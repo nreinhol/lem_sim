0635d83cf289d3b5402f734080a75366f0248429
Niklas Reinhold <nreinhold24@gmail.com>
2019-04-09 18:10:49 +0200

    adjust decomposition to new optimization problem

--- pyapp/src/lem_sim/linearoptimization/decompose.py
+++ pyapp/src/lem_sim/linearoptimization/decompose.py
@@ -17,6 +17,7 @@ def decompose(central_problem, var):
         for d_j, n_j, N_j, c_j, C_j, agent_j in zip(d, n, N, c, C, var.agent_pool):
                 optimization_problem_j = lp.OptimizationProblem(d_j, n_j, N_j, c_j, C_j)
                 agent_j.optimization_problem = optimization_problem_j
+                print(agent_j.bundle_set, agent_j.bid)
 
 
 def split_central_problem(central_problem, amount_agents):
@@ -36,11 +37,3 @@ def remove_zero_rows_of_individual_coefs(N):
 def distribute_shared_resources(c, amount_agents):
         ''' distribute the shared resources into equally sized parts '''
         return [np.divide(c, amount_agents + 1) for i in range(amount_agents)]
-
-
-def build_constraint_coefs_for_agents(N, C):
-        return [np.concatenate((N_j, C_j)) for N_j, C_j in zip(N, C)]
-
-
-def build_constraint_bounds_for_agents(n, c):
-        return [np.concatenate((n_j, c_j)) for n_j, c_j in zip(n, c)]
