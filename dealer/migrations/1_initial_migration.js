2a2c35193b6fe3fb778147c3fa7b54bc38f3bbd1
Niklas Reinhold <nreinhold24@gmail.com>
2019-04-17 15:59:14 +0200

    add deployment of dealer contract

--- dealer/migrations/1_initial_migration.js
+++ dealer/migrations/1_initial_migration.js
@@ -1,8 +1,11 @@
 const Migrations = artifacts.require("Migrations");
 const Inbox = artifacts.require("Inbox");
+const Dealer = artifacts.require("Dealer");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations).then(function() {
-    return deployer.deploy(Inbox);
+    return deployer.deploy(Inbox).then(function() {
+    return deployer.deploy(Dealer);
+    })
   })
 };
