aeb0c30e265201f0e9c0628fac1d882fc27b0968
Niklas Reinhold <nreinhold24@gmail.com>
2019-04-18 16:02:12 +0200

    add dealer contract to variable class

--- pyapp/src/lem_sim/globalmemory/variables.py
+++ pyapp/src/lem_sim/globalmemory/variables.py
@@ -1,5 +1,6 @@
 from lem_sim import communication
 from lem_sim import agent
+from lem_sim import contract
 
 
 class Variables(object):
@@ -9,6 +10,7 @@ class Variables(object):
         self._accounts = self._web3.eth.accounts
         self._agent_pool = [agent.Agent(account, self._web3)for account in self._accounts]
         self._amount_agents = len(self._agent_pool)
+        self._dealer_contract = contract.ContractHandler(self._web3, 'Dealer.json')
 
     @property
     def web3(self):
@@ -25,3 +27,7 @@ class Variables(object):
     @property
     def amount_agents(self):
         return self._amount_agents
+
+    @property
+    def dealer_contract(self):
+        return self._dealer_contract
