6ad4e65038fcfea36fb15072709805391f72b86e
Niklas Reinhold <nreinhold24@gmail.com>
2019-05-01 19:21:01 +0200

    move central problem to variables

--- pyapp/src/lem_sim/globalmemory/variables.py
+++ pyapp/src/lem_sim/globalmemory/variables.py
@@ -1,15 +1,26 @@
+import numpy as np
+
 from lem_sim import communication
 from lem_sim import client
 from lem_sim import contract
+from lem_sim import linearoptimization as lp
+
+''' Definition of the Central Optimization Problem '''
+TARGET_COEFS = np.array([-1, -2, -1, -3])  # cost vectors (d)
+INDIVIDUAL_RESOURCES = np.array([4, 9])  # individual resources (n)
+INDIVIDUAL_COEFS = np.array([[2, 1, 0, 0], [0, 0, 2, 3]])  # individual coefficients(N)
+SHARED_RESOURCES = np.array([8, 5])  # shared resources (c)
+SHARED_COEFS = np.array([[1, 3, 2, 1], [1, 1, 1, 1]])  # shared coefficients (C)
 
 
 class Variables(object):
 
     def __init__(self, connection):
+        self._central_problem = lp.OptimizationProblem(TARGET_COEFS, INDIVIDUAL_RESOURCES, INDIVIDUAL_COEFS, SHARED_RESOURCES, SHARED_COEFS)
         self._web3 = communication.get_network_connection(connection)
         self._dealer_contract = contract.ContractHandler(self._web3, 'Dealer.json')
         self._accounts = self._web3.eth.accounts
-        self._dealer = client.Dealer(self._accounts.pop(0), self._web3, self._dealer_contract)
+        self._dealer = client.Dealer(self._accounts.pop(0), self._web3, self._dealer_contract, self._central_problem.shared_resources.size)
         self._agent_pool = [client.Agent(account, self._web3, self._dealer_contract) for account in self._accounts]
         self._amount_agents = len(self._agent_pool)
 
@@ -36,3 +47,7 @@ class Variables(object):
     @property
     def dealer(self):
         return self._dealer
+
+    @property
+    def central_problem(self):
+        return self._central_problem
