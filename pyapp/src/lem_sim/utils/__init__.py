a703c4c04f68a909148c7910fa8c0240356339cb
Niklas Reinhold <nreinhold24@gmail.com>
2019-05-02 13:17:16 +0200

    add functions for sending and getting transactions

--- pyapp/src/lem_sim/utils/__init__.py
+++ pyapp/src/lem_sim/utils/__init__.py
@@ -1,2 +1,4 @@
 from .contract_communication import shift_decimal_right  # noqa
 from .contract_communication import shift_decimal_left  # noqa
+from .contract_communication import prepare_for_sending  # noqa
+from .contract_communication import prepare_for_storing  # noqa
--- pyapp/src/lem_sim/utils/contract_communication.py
+++ pyapp/src/lem_sim/utils/contract_communication.py
@@ -1,5 +1,23 @@
+import numpy as np
+
 DECIMAL_PLACES = 10
 MULTIPLIER = 10**DECIMAL_PLACES
+
+
+def prepare_for_sending(value):
+    if type(value) is np.ndarray:
+        value = value.tolist()
+        return shift_decimal_right(value)
+    else:
+        return shift_decimal_right(value)
+
+
+def prepare_for_storing(value):
+    if type(value) is list:
+        value = shift_decimal_left(value)
+        return np.array(value)
+    else:
+        return shift_decimal_left(value)
 
 
 def shift_decimal_right(value):
