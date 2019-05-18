de1adf536a999e92a9675426d3ffe50e62910f86
Niklas Reinhold <nreinhold24@gmail.com>
2019-05-18 16:32:08 +0200

    get all events

--- pyapp/src/lem_sim/analyse/event_handler.py
+++ pyapp/src/lem_sim/analyse/event_handler.py
@@ -3,11 +3,11 @@ from lem_sim import contract
 
 
 class EventHandler(object):
-    
+
     def __init__(self):
         self._web3 = communication.get_network_connection()
         self._dealer_contract = contract.ContractHandler(self._web3, 'Dealer.json')
-    
+
     def filter_received_order_event(self, from_block, to_block):
         event_filter = self._dealer_contract.contract.events.ReceivedOrder.createFilter(fromBlock=from_block, toBlock=to_block)
         return event_filter.get_all_entries()
--- pyapp/src/lem_sim/analyse/run.py
+++ pyapp/src/lem_sim/analyse/run.py
@@ -4,14 +4,31 @@ from lem_sim import analyse
 
 
 @c.command()
-@c.option('--analyse', type=c.Choice(['events']))
-def run(analyse):
-    event_handler = analyse.EventHandler()
-    eventlist = event_handler.filter_received_order_event(0, 'latest')
-    
-    for event in eventlist:
-        print(event['args'], event['blockNumber'])
+@c.option('--option', type=c.Choice(['events']))
+def run(option):
 
+    if(option == 'events'):
+        event_handler = analyse.EventHandler()
+        received_order_events = event_handler.filter_received_order_event(0, 'latest')
+        stored_trade_events = event_handler.filter_stored_trade_event(0, 'latest')
+        fetched_trade_events = event_handler.filter_fetched_trade_event(0, 'latest')
+        rejected_trade_events = event_handler.filter_rejected_trade_event(0, 'latest')
+
+        print('\nRECEIVED ORDERS')
+        for event in received_order_events:
+                print(event['args'], event['blockNumber'])
+
+        print('\nSTORED TRADES')
+        for event in stored_trade_events:
+                print(event['args'], event['blockNumber'])
+
+        print('\nFETCHED TRADES')
+        for event in fetched_trade_events:
+                print(event['args'], event['blockNumber'])
+
+        print('\nREJECTED TRADES')
+        for event in rejected_trade_events:
+                print(event['args'], event['blockNumber'])
 
 if __name__ == '__main__':
     run()
