d9302773710a8e79198d4c7706bd6733dd091790
Niklas Reinhold <nreinhold24@gmail.com>
2019-05-23 15:17:15 +0200

    add functions for trade account handling

--- dealer/contracts/Dealer.sol
+++ dealer/contracts/Dealer.sol
@@ -16,8 +16,12 @@ contract Dealer{
     mapping(uint32 => Order) public orders;
 
     // attributes for trade handling
-    address[] trades_LUT;
-    mapping(address => int256[]) private trades;
+
+    // mapping from address to pisition in trades_accounts array
+    // 0 means the address in not in the array
+    mapping (address => uint256) account_index; 
+    address[] public trades_accounts;
+    mapping(address => int256[]) public trades;
     mapping(address => uint256) public bills;
     mapping(address => uint256) public prepayments;
     mapping(address => uint256) public refunds;
@@ -38,6 +42,7 @@ contract Dealer{
     constructor() public {
         _owner = msg.sender;
         order_count = 1;
+        trades_accounts.push(0x0);
     }
 
     /*
@@ -60,6 +65,10 @@ contract Dealer{
         _;
     }
 
+    /*
+    custom structs
+    */
+
     struct Order {
         address account;
         int256[] bundle;
@@ -70,8 +79,23 @@ contract Dealer{
     functions for trade handling
     */ 
 
+    function addToArray(address who) {
+        if(!inArray(who)) {
+            //append
+            account_index[who] = trades_accounts.length;
+            trades_accounts.push(who);
+        }
+    }
+
+    function inArray(address who) public view returns (bool) {
+        if(who != 0x0 && account_index[who] > 0) {
+            return true;
+        }
+        return false;
+    }
+
     function setTrade(address _account, int256[] _trade, uint256 _prepayment, uint256 _bill, uint256 _refund) public onlyByOwner() {
-        trades_LUT.push(_account);
+        addToArray(_account);
         trades[_account] = _trade;
         bills[_account] = _bill;
         prepayments[_account] = _prepayment;
@@ -80,7 +104,12 @@ contract Dealer{
         emit StoredTrade(_account, _trade, _prepayment, _bill, _refund);
     }
 
-    function getTrade(bool accept_trade) public returns (int256[]) {
+    function getTrade(address account) public returns (int256[]) {
+        return trades[account];
+        
+    }
+
+    function acceptTrade(bool accept_trade) public returns (int256[]) {
         if(accept_trade) {
             msg.sender.transfer(refunds[msg.sender]);
             emit FetchedTrade(msg.sender, trades[msg.sender], refunds[msg.sender]);
@@ -149,12 +178,6 @@ contract Dealer{
         return resource_inventory;
     }
 
-    function calculateResourceInventory() public view {
-        for(uint i=0; i<trades_LUT.length; i++) {
-
-        }
-    }
-
     /*
     getter and setter for mmp attributes
     */
@@ -184,6 +207,10 @@ contract Dealer{
 
     function getOrderIndices() public view returns (uint32[]) {
         return order_indices;
+    }
+
+    function getTradesAccounts() public view returns (address[]) {
+        return trades_accounts;
     }
 
 }
